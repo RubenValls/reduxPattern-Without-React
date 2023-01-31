c6e4c5594c7b970fa402676375fcf1e3b16a0d38
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-31 12:34:59 +0100

    adding first docs

---
+++ app.js
@@ -0,0 +1,19 @@
+import store from './store.js';
+import { add, reduce } from './actions.js';
+
+const default_state = 0;
+
+// Puedes suscribirte manualmente a los cambios, o conectar tu vista
+// directamente
+store.subscribe(() => {
+    console.log(store.getState())
+});
+
+// La única forma de modificar el estado interno es despachando acciones.
+// Las acciones pueden ser serializadas, registradas o almacenadas luego para
+// volver a ejecutarlas.
+store.dispatch(add(3));
+
+store.dispatch(reduce(1));
+
+store.dispatch(add(7));
---
+++ reducer.js
@@ -0,0 +1,23 @@
+/**
+ * Esto es un reducer, una función pura con el formato
+ * (state, action) => newState. Describe como una acción transforma el estado
+ * en el nuevo estado.
+ *
+ * La forma del estado depende de tí: puede ser un primitivo, un array, un
+ * objeto, o incluso una estructura de datos de Immutable.js. La única parte
+ * importante es que no debes modificar el objeto del estado, en vez de eso
+ * devolver una nuevo objeto si el estado cambió.
+ *
+ * En este ejemplo, usamos `switch` y strings, pero puedes usar cualquier forma
+ * que desees si tiene sentido para tu proyecto.
+ */
+export function counter(state = 0, action) {
+    switch (action.type) {
+    case 'ADD':
+        return state + action.number;
+    case 'REDUCE':
+        return state - action.number;
+    default:
+        return state;
+    }
+}
